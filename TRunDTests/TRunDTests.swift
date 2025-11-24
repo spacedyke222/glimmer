f57db577c775242fe2d514e386797875ec2798e4
Lauren Harrell <design.laurenashley@gmail.com>
2025-11-24 17:01:26 -0500

    Initial Commit

---
+++ TRunDTests/TRunDTests.swift
@@ -0,0 +1,16 @@
+//
+//  TRunDTests.swift
+//  TRunDTests
+//
+//  Created by Bitch Bag 1 on 11/24/25.
+//
+
+import Testing
+
+struct TRunDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
